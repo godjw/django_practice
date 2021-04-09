b0a5441f41258f0a353d908201d9f0a6d46fefac
Ubuntu <ubuntu@ip-172-31-40-224.ap-northeast-2.compute.internal>
2021-04-09 04:49:41 +0000

    fix static problem

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,6 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'front', 'build', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
